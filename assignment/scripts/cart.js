c39b65cc01489b1f90632562d54b588d6716b339
Jenny Lagervall <jennylagervall@gmail.com>
2024-08-11 17:33:35 -0700

    finished first few Qs, starting challange Q's

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,43 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
+function addItem(item){
+basket.push(item);
+return true;
+}
+addItem('pretzels')
 
+console.log('should show items in basket array', basket)
+console.log(`Basket is ${basket}`);
+// end addItem
 
+function listItems(array){
+    for (let i = 0; i < array.length; i++)
+        console.log(array[i]) 
+}
+// end listItems
 
+function empty(array){
+    while(array.length > 0){
+        array.pop();
+    }
+}
+empty(basket);
+console.log('Basket array should be empty', basket);
+// end part 1
+
+Add a global const named maxItems and set it to 5.
+
+Create a function called isFull(). It should:
+
+return false if the basket contains less than max number of items
+return true otherwise (equal or more than maxItems)
+Update the required addItem function to:
+Use the isFull function to prevent more than maxItems from being added to the basket.
+If an item was added to the array, return true
+If there was no room and the item could not be added return false
 
 
 
